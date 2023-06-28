df4e6aa770117531190fc672a3add717da9ce03e
Shydeev <hday5297@gmail.com>
2023-06-28 12:42:57 +0300

    Featured Products.

--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -104,5 +104,14 @@
 
             return new ServiceResponse<List<string>> { Data = result };
         }
+
+        public async Task<ServiceResponse<List<Product>>> GetFeaturedProducts()
+        {
+            var response = new ServiceResponse<List<Product>> {
+                Data = await _context.Products.Where(p => p.Featured).Include(p => p.Variants).ToListAsync()
+            };
+
+            return response;
+        }
     }
 }
